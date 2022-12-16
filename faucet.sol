8e509bcc07d1ebed1dc61f5f0abd0e1fbb514245
TecY <81392920+hedgeme@users.noreply.github.com>
2022-12-16 09:02:24 -0600

    faucet.sol
    this is from the Book Mastering Ethereum

---
+++ faucet.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: CC-BY-SA-4.0
+pragma solidity ^0.6.4;
+
+contract Faucet {
+
+    address payable owner;
+
+    constructor () public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the contract owner can call this function");
+        _;
+    }
+
+    receive () external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    event Withdrawal (address indexed to, uint amount); // indexed to all searching and filtering in any user interface
+    event Deposit (address indexed from, uint amount);
+
+
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
+
+    function withdraw(uint withdraw_amount) public {
+        require (address(this).balance <= 0.1 ether);
+        msg.sender.transfer(withdraw_amount);
+        emit Withdrawal(msg.sender, withdraw_amount);
+    }
+
+
+ }
